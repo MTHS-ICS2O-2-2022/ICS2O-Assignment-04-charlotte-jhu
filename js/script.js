4f481d5db19ccdeeecc76f0fbe83b4c465d7a8cd
Patrick Coxall <mr.coxall@mths.ca>
2023-05-01 16:44:58 +0000

    try this

--- js/script.js
+++ js/script.js
@@ -7,13 +7,37 @@
 'use strict'
 
 function myButtonClicked() {
-  // input
-  const braceletLength = parseFloat(document.getElementById("bracelet-length").getAttribute('data-val'))
-  const numberOfColours = parseFloat(document.getElementById("number-of-colours").value)
   const TAX = 0.13
+  let lengthCost = 0.00
+  let colourCost = 0.00
+
+  // input
+  const braceletLength = parseFloat(document.getElementById("bracelet-length").value)
+  const numberOfColours = parseFloat(document.getElementById("number-of-colours").value)
 
   // process
-    const subtotal = braceletLength
+  if (braceletLength == "5 inches") {
+    lengthCost = 4.00
+  } else if (braceletLength == "6 inches") {
+    //
+  } else if (braceletLength == "7 inches") {
+    //
+  }
+
+  if (numberOfColours == "1 colour") {
+    colourCost = 0.50
+  } else if (numberOfColours == "2 colours") {
+    //
+  } else if (numberOfColours == "3 colours") {
+    //
+  }
+
+
+  let subtotal = lengthCost + colourCost
+
+
+
+  
   if (subtotal >= 6) {
     document.getElementById("special-message").innerHTML = ("Thank you for your purchase, you get a 10% discount next time!")
   }
