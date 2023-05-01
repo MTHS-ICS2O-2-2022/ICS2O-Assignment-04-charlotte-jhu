d7ee540eaa49965f87e76cd68390c10e477d0717
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-01 16:30:54 +0000

    added inital code

---
+++ go_app/main.go
@@ -0,0 +1,18 @@
+// Created by: Charlotte Jhu
+// Created on: April 2023
+//
+// This program calcualtes the price of your order from a bracelet store
+
+package main
+
+import ("fmt")
+
+func main() {
+	// variables
+	var braceletLength float64
+	var numberOfColours float64
+
+	// input
+	fmt.Println("Welcome to the bracelet store!")
+	fmt.Println("Please enter the length of your bracelet in inches: ")
+	// ask about selections for this 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
 // Created by: Charlotte Jhu
-// Created on: March 2023
+// Created on: April 2023
 // This file contains the JS functions for index.html
 
+'use strict'
+
 function myButtonClicked() {
-  alert("You clicked the button!");
+  // input
+  const braceletLength = parseFloat(document.getElementById("bracelet-length").getAttribute('data-val'))
+  const numberOfColours = parseFloat(document.getElementById("number-of-colours").value)
+  const TAX = 0.13
+
+  // process
+    const subtotal = braceletLength
+  if (subtotal >= 6) {
+    document.getElementById("special-message").innerHTML = ("Thank you for your purchase, you get a 10% discount next time!")
+  }
+  const tax = subtotal * TAX
+  const total = subtotal * (1 + TAX)
+
+  // output
+  document.getElementById("price").innerHTML =
+    "Subtotal: $" +
+    subtotal.toFixed(2) +
+    "<br>Tax: $" +
+    tax.toFixed(2) +
+    "<br>Total: $" +
+    total.toFixed(2)
 }
